4133a7a051cfed7d7b239f65435b8dbc5628d808
kdm <kmalinich@users.noreply.github.com>
2021-03-21 21:36:43 -0400

    Update

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-const exec = require('execa')
-const plist = require('plist')
+const exec  = require('execa');
+const plist = require('plist');
 
-const defaultOptions = ['-r', '-n']
+
+const defaultOptions = [ '-n', '-r' ];
+
 
 const ioreg = (className = 'Root', options = defaultOptions) => {
-  return new Promise((resolve, reject) => {
-    exec('ioreg', ['-a'].concat(options).concat(className))
-      .then(stdio => Promise.resolve(stdio.stdout))
-      .then(stdout => resolve(plist.parse(stdout)))
-      .catch(err => reject(err))
-  })
-}
+	return new Promise((resolve, reject) => {
+		exec('ioreg', ['-a'].concat(options).concat(className))
+			.then(stdio => Promise.resolve(stdio.stdout))
+			.then(stdout => resolve(plist.parse(stdout)))
+			.catch(err => reject(err));
+	});
+};
 
-module.exports = ioreg
+
+module.exports = ioreg;
